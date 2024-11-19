14154e95666e2e8f9986fcdd7fa7e6a6fa93615e
Mvzvrt <87718155+Mvzvrt@users.noreply.github.com>
2024-11-19 21:41:07 +0800

    Update README.md

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,8 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use('/api/evaluation', require('./routes/evaluation.routes'));
-app.use('/api/questions', require('./routes/question.routes'));
+app.use('/api/evaluation', require('./routes/evaluationRoutes'));
+// app.use('/api/questions', require('./routes/question.routes'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
